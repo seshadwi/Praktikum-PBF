d8abb81aa85e15560e60128ca1f0c7a06ad3ffe6
seshadwi <seshaadwi@gamil.com>
2022-04-18 16:37:33 +0700

    Tugas 9

---
+++ Tugas 9/src/services/index.js
@@ -0,0 +1,15 @@
+import GetAPI from "./Get";
+import PostAPI from "./Post";
+import DeleteAPI from "./Delete"
+
+const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
+const postNewsBlog = (dataYgDikirim) => PostAPI('posts', dataYgDikirim);
+const deleteNewsBlog = (dataYgDihapus) => DeleteAPI('posts', dataYgDihapus);
+
+const API = {
+    getNewsBlog,
+    postNewsBlog,
+    deleteNewsBlog
+}
+
+export default API;
